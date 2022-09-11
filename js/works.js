fb022f276cf09ab3a9e8fbcec1f4f47cc2d1f721
Alejandro Barrera <alejandro91dark@hotmail.es>
2022-09-11 18:44:05 -0300

    se agrego funcion que permite seleccionar categoria de palabras

--- js/works.js
+++ js/works.js
@@ -1,27 +1,31 @@
-let words = [
-    'software',
-    'hardware',
-    'monitor',
-    'teclado',
-    'mouse',
-    'impresora',
-    'procesador',
-    'ventilador',
-    'control',
-    'perro',
-    'gato',
-    'cerdo',
-    'lobo',
-    'mosca',
-    'delfin',
-    'tiburon',
-    'sekiro',
-    'darksoul',
-    'bloodborne',
-    'eldenring',
-    'javascript',
-    'html',
-    'vscode',
-    'hipertexto',
-    'enlace',
-];
+// SelectCategory:
+// 1: Informactica
+// 2: frutas
+// 3: animales
+// 4: videojuegos
+// 5: autos
+let words = [];
+
+function selectCategory(option){
+    words = [];
+
+    switch(option){
+        case 1:
+            words = ['software','hardware','monitor','javascript','vscode','teclado','impresora','hipertexto','phyton','enlace'];
+        break;
+        case 2:
+            words = ['manzana','pera','platano','frutilla','piña','ciruela','melon','sandia','papaya','mango'];
+        break;
+        case 3:
+            words = ['perro','gato','raton','lobo','zorro','gorila','elefante','hiena','jaguar','canguro'];
+        break;
+        case 4:
+            words = ['sekiro','darksoul','bloodborne','eldenring','mariobros','roblox','minecraft','bioschok','rayman','megaman'];
+        break;
+        case 5:
+            words = ['mazda','chevrolet','ferrari','audi','volvo','ford','hyundai','nissan','maserati','peugeot'];
+        break;
+    }
+}
+
+selectCategory(1);
